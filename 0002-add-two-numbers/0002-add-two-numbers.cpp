51527bbe45e32762b0d934dadc8468026619b791
Rahul Shivhare <110449333+RahulShivhare44@users.noreply.github.com>
2023-08-20 22:14:32 +0530

    Time: 32 ms (59.97%), Space: 71.4 MB (70.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -16,7 +16,7 @@ public:
         int carry = 0;
 
         while(l1 != nullptr || l2 != nullptr || carry != 0){
-             int digit1 = (l1 != nullptr) ? l1->val : 0;
+            int digit1 = (l1 != nullptr) ? l1->val : 0;
             int digit2 = (l2 != nullptr) ? l2->val : 0;
 
             int sum = digit1 + digit2 + carry;
