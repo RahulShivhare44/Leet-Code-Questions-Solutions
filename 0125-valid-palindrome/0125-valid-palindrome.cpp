6d0e7c0629c3c0809a7d372822ea2f0a2bf60320
Rahul Shivhare <110449333+RahulShivhare44@users.noreply.github.com>
2023-08-25 00:14:18 +0530

    Time: 4 ms (74.09%), Space: 7.5 MB (48.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -11,20 +11,15 @@ public:
                 ans += s[i];
             }
         }
-
-        cout<< ans;
-
+        
         int i = 0;
         int j = ans.length() - 1;
-
         while(i <= j){
             if(ans[i] != ans[j]){
                return false;
             }
-            else{
-                i++;
-                j--;
-            }
+            i++;
+            j--;
         }
         return true;
     }
